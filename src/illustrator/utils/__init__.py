7b122f9ca48151fdd2e64b94be9386dca8c8a64c
fortunestoldco <david@othertales.co>
2025-09-27 23:51:15 +0100

    Update __init__.py

--- src/illustrator/utils/__init__.py
+++ src/illustrator/utils/__init__.py
@@ -1,4 +1,7 @@
 """Utility functions for the illustrator application."""
 
-from .validation_helpers import ensure_chapter_required_fields
-from ..utils import parse_llm_json
+from .validation_helpers import (
+    ensure_chapter_required_fields,
+    validate_manuscript_before_save,
+    parse_llm_json
+)
