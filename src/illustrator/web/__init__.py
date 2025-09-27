36019c675a707812fb99902c2c89c8ba8bc0abde
fortunestoldco <david@othertales.co>
2025-09-27 23:52:55 +0100

    Fixes

--- src/illustrator/web/__init__.py
+++ src/illustrator/web/__init__.py
@@ -1,1 +1,5 @@
 """Web interface for the Manuscript Illustrator."""
+
+# Make routes accessible through the package
+from . import routes
+from . import models
--- src/illustrator/web/routes/__init__.py
+++ src/illustrator/web/routes/__init__.py
@@ -1,1 +1,5 @@
 """API routes for the web interface."""
+
+# Make route modules accessible
+from . import manuscripts
+from . import chapters
